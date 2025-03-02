abdf101b2b5b1fcb0aa926b3eca50689f5836fe9
farrel irawan <irawanssfarrel@gmail.com>
2025-03-02 15:42:52 +0700

    merge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,17 @@
 import React from "react"
 import "./globals.css"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "MarketSight",
   description: "Search your medicine",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+type RootLayoutProps = {
+  children: React.ReactNode
 }
 
-export default function RootLayout({ children } : LayoutProps) {
+export default function RootLayout({ children } : RootLayoutProps) {
   return (
     <html lang="en">
       <body>
